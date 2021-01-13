c9b309162f65af815326b7984bc319f2ee86549c
ozkanakkaya <ozkanakkaya@hotmail.com.tr>
2021-01-13 17:39:30 +0300

    startup.cs dosyası güncellendi. Dependency injection ayarları yapıldı. ConnectionString ayarlandı.

--- UdemyNLayerProject.Web/Startup.cs
+++ UdemyNLayerProject.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+using UdemyNLayerProject.Core.Services;
+using UdemyNLayerProject.Core.UnitOfWorks;
+using UdemyNLayerProject.Data;
+using UdemyNLayerProject.Data.Repositories;
+using UdemyNLayerProject.Data.UnitOfWorks;
+using UdemyNLayerProject.Service.Services;
 
 namespace UdemyNLayerProject.Web
 {
@@ -23,6 +31,32 @@ namespace UdemyNLayerProject.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Dependency Injection ayarlarý
+            //Eðer constructor da IRepository ile karþýlaþýrsan git Reepository den bir nesne örneði oluþtur ve IRepository e ata.
+
+            //AddScoped ýn yaptýðý iþlem: Bir istek esnasýnda, bir classýn ctor unda bir interface ile karþýlaþýrsa, ona karþýlýk gelen classa giderek bir nesne örneði oluþturur.
+            //Ayný reques içerisinde birden fazla karþýlaþýrsa ilk oluþtuduðu nesne örneðini alýr.
+            //Eðer her karþýlaþtýðýnda yeni bir nesne örneði oluþturmasý için "AddTransient" i kullanabiliriz. Ama AddScoped daha performanslýdýr.
+            // "AddSingleton" ise sadece tek birkez oluþturur ve uygulama boyunca oluþan o class ý kullanýr.
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IService<>), typeof(Service.Services.Service<>));
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                //EntityFramework ün kullanacaðý veritabaný(splserver) ve baðlantýsý verildi.
+                options.UseSqlServer(Configuration
+                    ["ConnectionStrings:SqlConStr"].ToString(), o =>
+                    {
+                        o.MigrationsAssembly("UdemyNLayerProject.Data");
+                    });
+            });
+
+
             services.AddControllersWithViews();
         }
 
